d30cc6a70d8fb99275d1310df0c6d765e7be15c9
Xavier Juan Serrador <xavier.juan@scmspain.com>
2017-01-26 12:11:07 +0100

    add readme

--- src/Controller.py
+++ src/Controller.py
@@ -4,13 +4,15 @@ import sys
 from application.RegexGenerator import RegexGenerator
 
 INFORMATION_MESSAGE = "You must specify a numbers list comma separated, like: 123456,456789,456789"
+NUMBERS_DELIMITER = ","
+
 if len(sys.argv) < 2:
     raise ValueError(INFORMATION_MESSAGE)
 
 regex_generator = RegexGenerator()
 
 try:
-    phones_list = list(map(int, str(sys.argv[1]).split(',')))
+    phones_list = list(map(int, str(sys.argv[1]).split(NUMBERS_DELIMITER)))
 except ValueError:
     raise ValueError(INFORMATION_MESSAGE)
 
